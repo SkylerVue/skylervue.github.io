ff52884c92153df208709f0fc7a70a0b19562e10
SkylerVue <97487457+SkylerVue@users.noreply.github.com>
2022-04-13 18:52:52 -0500

    Updated to fit midterm standards
    Added css, edited javascript to move pictures, updated html to use css

--- sketch.js
+++ sketch.js
@@ -1,8 +1,13 @@
-let img1;
+let cloud, websiteHeading, fakeNFT, logo;
+let ad1, ad2, ad3, ad1a, ad2a, ad3a;
+let elonMuskTweet, heading1, heading2, websiteTitle;
+let txt1, txt2, nftCaption, elonTweetCaption, ads, ads1;
+
 
 function preload(){
+
+  logo = createImg("Images/logo(FINAL).png", "logo");
   cloud = createImg("Images/Cloud.png", "A cloud");
-  websiteHeading = createP("Rain drop sized news");
   fakeNFT = createImg("Images/FakeNFT.png", "World's most expensive NFT");
   ad1 = createImg("Images/ad1.png", "Ad");
   ad2 = createImg("Images/ad2.png", "Ad");
@@ -13,7 +18,6 @@ function preload(){
   elonMuskTweet = createImg("Images/FAKEElonMuskTweet.png", "A tweet from Elon Musk that says \"Bitcoin will drop! Try to sell your coins to someone else if you can! Buy Dogecoin instead!\"");
   heading1 = createP("Most Expensive NFT Sold!");
   heading2 = createP("Bitcoin Projected to Drop Even More!");
-  websiteTitle = createP("Sky News");
   txt1 = createP("This week, the most expensive NFT has been sold. The NFT named \"Smile Boi\" has been sold for $3.9 million. This life changing NFT has such a heavy price to it since it has been created AND signed by the world famous artist, \"Suewoop ヅ\". Everyone is now trying to reach for the stars to get this priceless NFT.");
   txt2 = createP("Bitcoin has dropped to the lowest it has been in awhile. It is projected to drop even lower than it is now. This has caused many people to lose trust in Bitcoin. The drop in Bitcoin may have stemmed from this Tweet from Elon Musk (screenshot on the right). Though it is not confirmed, Mr. Musk does have a large following and has enough influence to cause such an impact.")
   nftCaption = createP("\"Smile Boi\" by Suewoopヅ");
@@ -23,86 +27,29 @@ function preload(){
   ads1 = createP("Ads");
 }
 
+
 function setup() {
-  createCanvas(1519, 1080);
 
-  cloud.position(487, 0);
+  cloud.parent('top');
+  logo.parent("topLeft");
 
-  websiteHeading.style("font-size", "25px");
-  websiteHeading.size(500, 50);
-  websiteHeading.position(650, 150);
+  ads.parent('left');
+  ad1.parent('left');
+  ad2.parent('left');
+  ad3.parent('left');
 
-  websiteTitle.size(300, 300)
-  websiteTitle.style("font-size", "50px");
-  websiteTitle.position(670 , 10);
+  ads1.parent('right');
+  ad1a.parent('right');
+  ad2a.parent('right');
+  ad3a.parent('right');
 
-  heading1.style("font-size", "35px");
-  heading1.size(500, 40);
-  heading1.position(550,205);
+  fakeNFT.parent('mainBodyRight');
+  nftCaption.parent('mainBodyRight');
+  elonMuskTweet.parent('mainBodyRight');
+  elonTweetCaption.parent('mainBodyRight');
 
-  fakeNFT.size(300, 164);
-  fakeNFT.position(1000, 300)
-
-  txt1.size(750, 450);
-  txt1.style("font-size", "20px");
-  txt1.position(230, 310);
-
-  nftCaption.style("font-size", "15px");
-  nftCaption.size(500, 50);
-  nftCaption.position(1050, 455);
-
-  heading2.style("font-size", "35px");
-  heading2.size(700, 40);
-  heading2.position(500,610);
-
-  txt2.size(750, 450);
-  txt2.style("font-size", "20px");
-  txt2.position(230, 715);
-
-  elonMuskTweet.size(300, 164);
-  elonMuskTweet.position(1000, 725);
-
-  elonTweetCaption.style("font-size", "15px");
-  elonTweetCaption.size(500, 50);
-  elonTweetCaption.position(1050, 880);
-
-
-  ads.style("font-size", "15px");
-  ads.position(91, 200);
-
-  ads1.style("font-size", "15px");
-  ads1.position(1413, 200);
-
-  ad1.size(180, 120);
-  ad1.position(17, 250);
-
-  ad2.size(180, 120);
-  ad2.position(17, 475);
-
-  ad3.size(180, 120);
-  ad3.position(17, 700);
-
-  ad1a.size(180, 120);
-  ad1a.position(1335, 250);
-
-  ad2a.size(180, 120);
-  ad2a.position(1335, 475);
-
-  ad3a.size(180, 120);
-  ad3a.position(1335, 700);
-}
-
-function draw() {
-  background(255, 255, 255);
-  //background(140, 216, 255);
-
-  stroke(102);
-  fill(230, 230, 230);
-  rect(1319, 0, 200, 1080)
-  rect(0, 0, 200, 1080)
-
-
-  fill(140, 216, 255);
-  rect(0, 0, 1519, 200)
-
+  heading1.parent('mainBody');
+  txt1.parent('mainBody');
+  heading2.parent('mainBody');
+  txt2.parent('mainBody');
 }
