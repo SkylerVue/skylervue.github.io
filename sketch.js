a3a4876b1b174989fa80a25ba51200258f168e6b
SkylerVue <97487457+SkylerVue@users.noreply.github.com>
2022-04-23 18:16:55 -0500

    Updated sketch.js to access file location

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function preload(){
   a = createA('Pirate2(Copy_2).html', '<img src = "Images/shipBritish.png" alt="pirate ship">', '_blank');
   title1 = createP('Pirates and Navy Game');
 
-  b = createA('mosueMovement', '<img src = "mouseMovement/smile.jpg" alt="smile">', '_blank');
+  b = createA('mouseMovement/index.html', '<img src = "mouseMovement/smile.jpg" alt="smile">', '_blank');
   title2 = createP('Random Rectangles');
 
   heading = createP("Welcome to my website");
